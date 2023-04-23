a6804eaeb42ef6ce7b5f66acaf730938cc94b805
Bryan Zapata <bmzc95@gmail.com>
2023-04-23 15:34:47 -0500

    debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: const Color(0xff0F0F0F),
